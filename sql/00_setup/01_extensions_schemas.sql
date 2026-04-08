2acf8335455050d0bc75cfe85b411b1adce2e625
sumikara <63916396+sumikara@users.noreply.github.com>
2026-04-08 23:26:33 +0300

    Update 01_extensions_schemas.sql

--- sql/00_setup/01_extensions_schemas.sql
+++ sql/00_setup/01_extensions_schemas.sql
@@ -1,1 +1,18 @@
---setup script placeholder
+-- Extensions
+CREATE EXTENSION IF NOT EXISTS file_fdw;
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+-- Foreign server (csv_server)
+DO $$
+BEGIN
+  IF NOT EXISTS (SELECT 1 FROM pg_foreign_server WHERE srvname = 'csv_server') THEN
+    CREATE SERVER csv_server FOREIGN DATA WRAPPER file_fdw;
+  END IF;
+END $$;
+
+-- Schemas
+CREATE SCHEMA IF NOT EXISTS sl_online_retail; -- sl:source layer
+CREATE SCHEMA IF NOT EXISTS sl_offline_retail;
+CREATE SCHEMA IF NOT EXISTS stg; -- staging
+CREATE SCHEMA IF NOT EXISTS 3nf; -- normalized, Immon
+CREATE SCHEMA IF NOT EXISTS dim; -- denormalized, Kimball
